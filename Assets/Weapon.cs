cfd7cefca1bf9b8dd29d1ce9c9c7bdccabb17e90
Benjamin Jensen <Benjaminmargon@hotmail.com>
2021-02-22 08:30:45 +0100

    Create project stucture

---
+++ Assets/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public float Speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot () {
+        GameObject spawn = Instantiate(bulletPrefab) as GameObject;
+        spawn.transform.position = FirePoint.transform.position;
+        spawn.GetComponent<Rigidbody2D>().velocity = FirePoint.transform.right * Speed;
+    }
+
+}
